de11b2c84157a2c6e5d66c0add501772f6b4e385
Zhimovskij <zhimovskij@yandex.ru>
2020-01-24 15:44:18 +0500

    add vue.js + babel + style-loader + css-loader + clean-webapck-plugin and configuring project for vue.js files

--- app/Views/page.php
+++ app/Views/page.php
@@ -1,1 +1,5 @@
-<p> Text TExt TExt Text Teext</p>
+<div id="app">
+    <p>{{ message }}</p>
+</div>
+<script src="<?= base_url(); ?>/js/vue.min.js"></script>
+<script src="<?= base_url(); ?>/dist/main.bundle.js"></script>
